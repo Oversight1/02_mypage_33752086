19571b671c6522c5911a9d9109de1d660ce8e6d5
Oversight1 <Hallshooting@gmail.com>
2025-10-07 16:53:10 +0100

    Week 1 lab work

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+const port = 8000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port, function() {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
+
