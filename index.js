54d607c267cb8c3c84810d1a218e4fa258e4a533
Oversight1 <Hallshooting@gmail.com>
2025-11-28 08:05:14 +0000

    Final change for visual

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello World -- Final change\n');
 }).listen(port, function() {
     console.log(`Server running at http://localhost:${port}/`);
 });
